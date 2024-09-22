c631fb38f00789875709607e60f06a1f8a8742af
ayrtonSmach <ayrtonsoaresm@gmail.com>
2024-09-22 14:31:57 -0300

    add submit_post function, url for submit, card post div in index and Post class model

--- network/models.py
+++ network/models.py
@@ -4,3 +4,11 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+class Post(models.Model):
+    username = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField(max_length=500)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'Username: {self.username}\nContent: {self.content}\n{self.created_at}'
--- network/views.py
+++ network/views.py
@@ -4,12 +4,22 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
 
-from .models import User
-
+from .models import User, Post
 
 def index(request):
-    return render(request, "network/index.html")
+    return render(request, "network/index.html", {
+        "posts": Post.objects.all(),
+        "user": request.user
+    })
 
+def submit_post(request):
+    if request.method == "POST":
+
+        username = request.user
+        content = request.POST["post_content"]
+
+        Post(username=username, content=content).save()
+        return HttpResponseRedirect(reverse("index"))
 
 def login_view(request):
     if request.method == "POST":
